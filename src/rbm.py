d99303b186e2ae78301eb24d0b9e146577e1bb82
m10han <mohan.noolu@machool.com>
2018-07-22 00:11:06 -0700

    Clean up staging

--- src/rbm.py
+++ src/rbm.py
@@ -1,12 +1,3 @@
-"""This tutorial introduces restricted boltzmann machines (RBM) using Theano.
-
-Boltzmann Machines (BMs) are a particular form of energy-based model which
-contain hidden variables. Restricted Boltzmann Machines further restrict BMs
-to those without visible-visible and hidden-hidden connections.
-"""
-
-from __future__ import print_function
-
 import timeit
 
 try:
@@ -40,29 +31,6 @@ class RBM(object):
         numpy_rng=None,
         theano_rng=None
     ):
-        """
-        RBM constructor. Defines the parameters of the model along with
-        basic operations for inferring hidden from visible (and vice-versa),
-        as well as for performing CD updates.
-
-        :param input: None for standalone RBMs or symbolic variable if RBM is
-        part of a larger graph.
-
-        :param n_visible: number of visible units
-
-        :param n_hidden: number of hidden units
-
-        :param W: None for standalone RBMs or symbolic variable pointing to a
-        shared weight matrix in case RBM is part of a DBN network; in a DBN,
-        the weights are shared between RBMs and layers of a MLP
-
-        :param hbias: None for standalone RBMs or symbolic variable pointing
-        to a shared hidden units bias vector in case RBM is part of a
-        different network
-
-        :param vbias: None for standalone RBMs or a symbolic variable
-        pointing to a shared visible units bias
-        """
 
         self.n_visible = n_visible
         self.n_hidden = n_hidden
@@ -75,11 +43,6 @@ class RBM(object):
             theano_rng = RandomStreams(numpy_rng.randint(2 ** 30))
 
         if W is None:
-            # W is initialized with `initial_W` which is uniformely
-            # sampled from -4*sqrt(6./(n_visible+n_hidden)) and
-            # 4*sqrt(6./(n_hidden+n_visible)) the output of uniform if
-            # converted using asarray to dtype theano.config.floatX so
-            # that the code is runable on GPU
             initial_W = numpy.asarray(
                 numpy_rng.uniform(
                     low=-4 * numpy.sqrt(6. / (n_hidden + n_visible)),
@@ -125,8 +88,7 @@ class RBM(object):
         # **** WARNING: It is not a good idea to put things in this list
         # other than shared variables created in this function.
         self.params = [self.W, self.hbias, self.vbias]
-        # end-snippet-1
-
+        
     def free_energy(self, v_sample):
         ''' Function to compute the free energy '''
         wx_b = T.dot(v_sample, self.W) + self.hbias
@@ -135,71 +97,34 @@ class RBM(object):
         return -hidden_term - vbias_term
 
     def propup(self, vis):
-        '''This function propagates the visible units activation upwards to
-        the hidden units
-
-        Note that we return also the pre-sigmoid activation of the
-        layer. As it will turn out later, due to how Theano deals with
-        optimizations, this symbolic variable will be needed to write
-        down a more stable computational graph (see details in the
-        reconstruction cost function)
-
-        '''
         pre_sigmoid_activation = T.dot(vis, self.W) + self.hbias
         return [pre_sigmoid_activation, T.nnet.sigmoid(pre_sigmoid_activation)]
 
     def sample_h_given_v(self, v0_sample):
-        ''' This function infers state of hidden units given visible units '''
-        # compute the activation of the hidden units given a sample of
-        # the visibles
         pre_sigmoid_h1, h1_mean = self.propup(v0_sample)
-        # get a sample of the hiddens given their activation
-        # Note that theano_rng.binomial returns a symbolic sample of dtype
-        # int64 by default. If we want to keep our computations in floatX
-        # for the GPU we need to specify to return the dtype floatX
         h1_sample = self.theano_rng.binomial(size=h1_mean.shape,
                                              n=1, p=h1_mean,
                                              dtype=theano.config.floatX)
         return [pre_sigmoid_h1, h1_mean, h1_sample]
 
     def propdown(self, hid):
-        '''This function propagates the hidden units activation downwards to
-        the visible units
-
-        Note that we return also the pre_sigmoid_activation of the
-        layer. As it will turn out later, due to how Theano deals with
-        optimizations, this symbolic variable will be needed to write
-        down a more stable computational graph (see details in the
-        reconstruction cost function)
-
-        '''
         pre_sigmoid_activation = T.dot(hid, self.W.T) + self.vbias
         return [pre_sigmoid_activation, T.nnet.sigmoid(pre_sigmoid_activation)]
 
     def sample_v_given_h(self, h0_sample):
-        ''' This function infers state of visible units given hidden units '''
-        # compute the activation of the visible given the hidden sample
         pre_sigmoid_v1, v1_mean = self.propdown(h0_sample)
-        # get a sample of the visible given their activation
-        # Note that theano_rng.binomial returns a symbolic sample of dtype
-        # int64 by default. If we want to keep our computations in floatX
-        # for the GPU we need to specify to return the dtype floatX
         v1_sample = self.theano_rng.binomial(size=v1_mean.shape,
                                              n=1, p=v1_mean,
                                              dtype=theano.config.floatX)
         return [pre_sigmoid_v1, v1_mean, v1_sample]
 
     def gibbs_hvh(self, h0_sample):
-        ''' This function implements one step of Gibbs sampling,
-            starting from the hidden state'''
         pre_sigmoid_v1, v1_mean, v1_sample = self.sample_v_given_h(h0_sample)
         pre_sigmoid_h1, h1_mean, h1_sample = self.sample_h_given_v(v1_sample)
         return [pre_sigmoid_v1, v1_mean, v1_sample,
                 pre_sigmoid_h1, h1_mean, h1_sample]
 
     def gibbs_vhv(self, v0_sample):
-        ''' This function implements one step of Gibbs sampling,
-            starting from the visible state'''
         pre_sigmoid_h1, h1_mean, h1_sample = self.sample_h_given_v(v0_sample)
         pre_sigmoid_v1, v1_mean, v1_sample = self.sample_v_given_h(h1_sample)
         return [pre_sigmoid_h1, h1_mean, h1_sample,
@@ -207,40 +132,14 @@ class RBM(object):
 
     # start-snippet-2
     def get_cost_updates(self, lr=0.1, persistent=None, k=1):
-        """This functions implements one step of CD-k or PCD-k
-
-        :param lr: learning rate used to train the RBM
-
-        :param persistent: None for CD. For PCD, shared variable
-            containing old state of Gibbs chain. This must be a shared
-            variable of size (batch size, number of hidden units).
-
-        :param k: number of Gibbs steps to do in CD-k/PCD-k
-
-        Returns a proxy for the cost and the updates dictionary. The
-        dictionary contains the update rules for weights and biases but
-        also an update of the shared variable used to store the persistent
-        chain, if one is used.
-
-        """
-
+        
         # compute positive phase
         pre_sigmoid_ph, ph_mean, ph_sample = self.sample_h_given_v(self.input)
 
-        # decide how to initialize persistent chain:
-        # for CD, we use the newly generate hidden sample
-        # for PCD, we initialize from the old state of the chain
         if persistent is None:
             chain_start = ph_sample
         else:
             chain_start = persistent
-        # end-snippet-2
-        # perform actual negative phase
-        # in order to implement CD-k/PCD-k we need to scan over the
-        # function that implements one gibbs step k times.
-        # Read Theano tutorial on scan for more information :
-        # http://deeplearning.net/software/theano/library/scan.html
-        # the scan will return the entire Gibbs chain
         (
             [
                 pre_sigmoid_nvs,
@@ -253,16 +152,10 @@ class RBM(object):
             updates
         ) = theano.scan(
             self.gibbs_hvh,
-            # the None are place holders, saying that
-            # chain_start is the initial state corresponding to the
-            # 6th output
             outputs_info=[None, None, None, None, None, chain_start],
             n_steps=k,
             name="gibbs_hvh"
         )
-        # start-snippet-3
-        # determine gradients on RBM parameters
-        # note that we only need the sample at the end of the chain
         chain_end = nv_samples[-1]
 
         cost = T.mean(self.free_energy(self.input)) - T.mean(
@@ -288,8 +181,7 @@ class RBM(object):
                                                            pre_sigmoid_nvs[-1])
 
         return monitoring_cost, updates
-        # end-snippet-4
-
+        
     def get_pseudo_likelihood_cost(self, updates):
         """Stochastic approximation to the pseudo-likelihood"""
 
@@ -302,9 +194,6 @@ class RBM(object):
         # calculate free energy for the given bit configuration
         fe_xi = self.free_energy(xi)
 
-        # flip bit x_i of matrix xi and preserve all other bits x_{\i}
-        # Equivalent to xi[:,bit_i_idx] = 1-xi[:, bit_i_idx], but assigns
-        # the result to xi_flip, instead of working in place on xi.
         xi_flip = T.set_subtensor(xi[:, bit_i_idx], 1 - xi[:, bit_i_idx])
 
         # calculate free energy with bit flipped
@@ -320,35 +209,7 @@ class RBM(object):
         return cost
 
     def get_reconstruction_cost(self, updates, pre_sigmoid_nv):
-        """Approximation to the reconstruction error
-
-        Note that this function requires the pre-sigmoid activation as
-        input.  To understand why this is so you need to understand a
-        bit about how Theano works. Whenever you compile a Theano
-        function, the computational graph that you pass as input gets
-        optimized for speed and stability.  This is done by changing
-        several parts of the subgraphs with others.  One such
-        optimization expresses terms of the form log(sigmoid(x)) in
-        terms of softplus.  We need this optimization for the
-        cross-entropy since sigmoid of numbers larger than 30. (or
-        even less then that) turn to 1. and numbers smaller than
-        -30. turn to 0 which in terms will force theano to compute
-        log(0) and therefore we will get either -inf or NaN as
-        cost. If the value is expressed in terms of softplus we do not
-        get this undesirable behaviour. This optimization usually
-        works fine, but here we have a special case. The sigmoid is
-        applied inside the scan op, while the log is
-        outside. Therefore Theano will only see log(scan(..)) instead
-        of log(sigmoid(..)) and will not apply the wanted
-        optimization. We can not go and replace the sigmoid in scan
-        with something else also, because this only needs to be done
-        on the last step. Therefore the easiest and more efficient way
-        is to get also the pre-sigmoid activation as an output of
-        scan, and apply both the log and sigmoid outside scan such
-        that Theano can catch and optimize the expression.
-
-        """
-
+       
         cross_entropy = T.mean(
             T.sum(
                 self.input * T.log(T.nnet.sigmoid(pre_sigmoid_nv)) +
